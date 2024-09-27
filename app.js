acf80ba6b3f512c7efcaa93335301b755e6ba9b0
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-09-27 11:09:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+// Task No. 1
+// var str = "hello world";
+// var cap = str.slice(0, 1).toUpperCase() 
+// var cap2 = str.slice(5, 7).toUpperCase();
+// var small = str.slice(1, 5);
+// var small2 = str.slice(7, 11);
+
+// var capital1= cap+small;
+// var capital2 = cap2+small2;
+
+// console.log(capital1, capital2);
+
+
+//  Task No. 2
+
+// var str = "Saylani Mass It Training Program";
+
+// for(var i = 0; i < str.length; i++){
+//     if(str.slice(i, i + 8) == "Training"){
+//         console.log("Found!");
+//     }
+// }
+
+ 
